06ce12a881727b327544d0ddb81bbd48e28a14e2
Bittner322 <sandreyf1972@gmail.com>
2022-11-29 20:47:20 +0600

    FT-59: Cleaned todo string

--- common/src/commonMain/kotlin/com/finance_tracker/finance_tracker/core/ui/transactions/CommonTransactionList.kt
+++ common/src/commonMain/kotlin/com/finance_tracker/finance_tracker/core/ui/transactions/CommonTransactionList.kt
@@ -18,7 +18,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.finance_tracker.finance_tracker.core.common.DateFormatType
@@ -68,7 +67,6 @@ private fun TransactionsList(
         modifier = modifier.fillMaxSize(),
         contentPadding = PaddingValues(bottom = 96.dp)
     ) {
-        //TODO RESOLVE CONFLICTS
         items(transactions, key = { it.id }) { transactionModel ->
             if (transactionModel == null) return@items
 
